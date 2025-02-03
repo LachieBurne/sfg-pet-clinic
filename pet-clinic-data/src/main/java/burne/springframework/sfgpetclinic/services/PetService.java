92b126408ba4f86f0f701ac9ce486c2d1e0bf693
LachieBurne <lachlanburne072899@gmail.com>
2025-02-03 13:00:11 +1100

    feat: incremental commit to add interfaces

---
+++ pet-clinic-data/src/main/java/burne/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package burne.springframework.sfgpetclinic.services;
+
+import burne.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}

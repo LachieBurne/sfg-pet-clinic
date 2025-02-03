ac56ddffbd2c9e1f8848315f3aa9a9245be02b1a
LachieBurne <lachlanburne072899@gmail.com>
2025-02-03 14:58:08 +1100

    feat: Implement Map Based Service

---
+++ pet-clinic-data/src/main/java/burne/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package burne.springframework.sfgpetclinic.services.map;
+
+import burne.springframework.sfgpetclinic.model.Pet;
+import burne.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+}
